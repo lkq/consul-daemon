6b26abbeb470f17e24466441f4394f5d8e60c914
kliu <kingson708@gmail.com>
2018-04-29 14:17:59 +0800

    inject app dependency

--- smesh-linkerd/src/main/java/com/github/lkq/smesh/linkerd/App.java
+++ smesh-linkerd/src/main/java/com/github/lkq/smesh/linkerd/App.java
@@ -1,25 +1,28 @@
 package com.github.lkq.smesh.linkerd;
 
 import com.github.lkq.smesh.context.ContainerContext;
-import com.github.lkq.smesh.docker.DockerClientFactory;
-import com.github.lkq.smesh.docker.SimpleDockerClient;
 import com.github.lkq.smesh.linkerd.api.LinkerdController;
-import com.github.lkq.smesh.linkerd.context.LinkerdCommandBuilder;
-import com.github.lkq.smesh.linkerd.context.LinkerdContextFactory;
-import com.github.lkq.smesh.linkerd.routes.v1.LinkerdRoutes;
 import com.github.lkq.smesh.server.WebServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
+    private final LinkerdController linkerdController;
+    private ContainerContext context;
+    private final WebServer webServer;
 
     /**
      * application entry point, a place to put together different pieces and make it run
      *
+     * @param context
+     * @param linkerdController
+     * @param webServer
      */
-    public App() {
-
+    public App(ContainerContext context, LinkerdController linkerdController, WebServer webServer) {
+        this.linkerdController = linkerdController;
+        this.context = context;
+        this.webServer = webServer;
     }
 
     /**
@@ -27,13 +30,9 @@ public class App {
      *
      */
     public void start() {
-        LinkerdController controller = new LinkerdController(SimpleDockerClient.create(DockerClientFactory.get()));
-        LinkerdContextFactory contextFactory = new LinkerdContextFactory();
-        ContainerContext context = contextFactory.createDefaultContext().commandBuilder(new LinkerdCommandBuilder());
-        controller.stopAndRemoveExistingInstance(context.nodeName());
-        controller.startNewInstance(context);
-        controller.attachLogging(context.nodeName());
-        WebServer webServer = new WebServer(new LinkerdRoutes(), 8009);
+        linkerdController.stopAndRemoveExistingInstance(context.nodeName());
+        linkerdController.startNewInstance(context);
+        linkerdController.attachLogging(context.nodeName());
         webServer.start();
     }
 
--- smesh-linkerd/src/test/java/com/github/lkq/smesh/linkerd/LocalLauncher.java
+++ smesh-linkerd/src/test/java/com/github/lkq/smesh/linkerd/LocalLauncher.java
@@ -1,6 +1,14 @@
 package com.github.lkq.smesh.linkerd;
 
+import com.github.lkq.smesh.context.ContainerContext;
+import com.github.lkq.smesh.docker.DockerClientFactory;
+import com.github.lkq.smesh.docker.SimpleDockerClient;
+import com.github.lkq.smesh.linkerd.api.LinkerdController;
+import com.github.lkq.smesh.linkerd.context.LinkerdCommandBuilder;
+import com.github.lkq.smesh.linkerd.context.LinkerdContextFactory;
+import com.github.lkq.smesh.linkerd.routes.v1.LinkerdRoutes;
 import com.github.lkq.smesh.logging.JulToSlf4jBridge;
+import com.github.lkq.smesh.server.WebServer;
 import org.slf4j.Logger;
 
 public class LocalLauncher {
@@ -11,7 +19,12 @@ public class LocalLauncher {
     }
 
     private void start() {
-        App app = new App();
+        final LinkerdContextFactory contextFactory = new LinkerdContextFactory();
+        ContainerContext context = contextFactory.createDefaultContext().commandBuilder(new LinkerdCommandBuilder());
+        LinkerdController linkerdController = new LinkerdController(SimpleDockerClient.create(DockerClientFactory.get()));
+        WebServer webServer = new WebServer(new LinkerdRoutes(), 8009);
+
+        App app = new App(context, linkerdController, webServer);
 
         Runtime.getRuntime().addShutdownHook(new Thread(app::stop));
 
